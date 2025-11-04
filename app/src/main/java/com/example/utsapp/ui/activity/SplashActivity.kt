8adbacc89adab21e1f933c030b7c8cac95a748fd
nothappenhere <muhammad.rizky15@mhs.itenas.ac.id>
2025-11-05 02:10:05 +0700

    Adding app icon

--- app/src/main/java/com/example/utsapp/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/utsapp/ui/activity/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.utsapp.ui.fragment
+package com.example.utsapp.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,7 +6,6 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.utsapp.R
-import com.example.utsapp.ui.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState : Bundle?) {
